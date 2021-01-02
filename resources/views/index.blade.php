c9298524d2180354b9baf2ca6c10b1d34e33671c
Alex Cozma <alex.cozma1995@gmail.com>
2021-01-02 19:16:01 +0200

    Installing tailwind + creating first controller with route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h1 class="mt-20 text-2xl">Hello World</h1>
+    </body>
+</html>
