9a443497ff16d925034c09e33a733072f718af3a
Alex Cozma <alex.cozma1995@gmail.com>
2021-01-09 19:59:17 +0200

    Post-User relationship, form, migration etc.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::get();
+
+        return view('posts.index', ['posts' => $posts]);
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        auth()->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return back();
+    }
+
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,39 @@
 
     <div class="flex justify-center">
         <div class="w-8/12 rounded-lg bg-white p-6 mt-10">
-            POSTS
+            <form action="{{ route('posts') }}" method="post">
+                @csrf
+                <div class="mb-4">
+                    <label for="body" class="sr-only"></label>
+                    <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg" placeholder="Post something!"></textarea>
+
+                    @error('body')
+                        <div class="text-red-500 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                    <div>
+                        <button type="submit" class="bg-blue-500 mt-5 text-white px-4 py-2 font-medium rounded">Post</button>
+                    </div>
+
+                </div>
+            </form>
+
+            <div>
+                @if($posts->count())
+                    @foreach($posts as $post)
+                        <div class="mb-4">
+                            <a href="" class="font-semibold">{{ $post->user->username }} </a><span class="text-gray-600">{{ $post->created_at->diffForHumans() }}</span>
+                            <div class="mb-2">
+                                {{ $post->body }}
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div>There are no posts yet</div>
+                @endif
+            </div>
         </div>
     </div>
 
